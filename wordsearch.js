203ad75e6479d8429e845c1538e937ca2054ef73
Senayson <senaialema@gmail.com>
2021-06-02 21:01:59 +0000

    pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+  let result = [];
+  for (const y in matrix[0]) {
+    let temp = [];
+    for (const x in matrix) {
+      temp.push(matrix[x][y]);
     }
-}
+    result.push(temp);
+  }
+  return result;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+
+ const horizontalJoin = letters.map(ls => ls.join(''));
+ const verticalJoin = transpose(letters).map(ls => ls.join(''));
+
+ for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+
+
+ }
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true;
+
+
+    }
+
+    return false;
+};
+
+module.exports = wordSearch;
